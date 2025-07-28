a8b3e73b04c07b6eece643efe6370913cfb11f35
mping <mping@LAPTOP-AMKA2OU1>
2025-07-28 20:15:18 +0200

    Add project files.

---
+++ NtandazoCreativeHub/Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Razor.RuntimeCompilation;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Enable Razor runtime compilation
+builder.Services.AddControllersWithViews()
+    .AddRazorRuntimeCompilation();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
---
+++ NtandazoCreativeHub/wwwroot/js/site.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function () {
+    $(".flying-text").each(function (index) {
+        var el = $(this);
+        setTimeout(() => {
+            el.addClass("animate__animated animate__fadeInUp");
+        }, index * 300);
+    });
+});
